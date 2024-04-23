53103b1a2f044c1958fac737e78c30d7ac817437
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-04-23 11:28:55 +0530

    add host name of aws s3 bucket

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "pizza-catalog-service.s3.ap-south-1.amazonaws.com",
+            }
+        ],
+    }
+};
 
 export default nextConfig;
