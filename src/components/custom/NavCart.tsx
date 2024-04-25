d9d7fbbc54691072b9ad88d829c048261c451b8a
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-04-25 15:16:08 +0530

    navcart for showing the cart counT

---
+++ src/components/custom/NavCart.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ShoppingBasket } from "lucide-react"
+import { useAppSelector } from "@/lib/redux/hooks"
+
+const NavCart = () => {
+    const cartItems = useAppSelector((state) => state.cart.cartItems)
+    return (
+        <div className="cursor-pointer relative hover:text-primary">
+            <ShoppingBasket />
+            <span className="sr-only">CartItems</span>
+            <div className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-medium text-white bg-primary border-2 border-white rounded-full -top-3 p-2 -end-2 dark:border-gray-900">
+                {cartItems.length || 0}
+            </div>
+        </div>
+    )
+}
+
+export default NavCart
