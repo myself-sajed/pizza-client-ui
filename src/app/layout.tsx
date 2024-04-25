27d0d7b73f2e63d696670670c991c6e765773a06
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-04-25 14:15:03 +0530

    setup react redux: a state management library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Nav from "@/components/custom/Nav";
 import { ReactQueryClientProvider } from "@/components/custom/ReactQueryClientProvider";
+import StoreProvider from "./StoreProvider";
 
 const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope", });
 
@@ -21,18 +22,20 @@ export default function RootLayout({
   return (
     <ReactQueryClientProvider>
       <html lang="en">
-        <body className={cn(
-          "min-h-screen bg-background font-manrope antialiased",
-          manrope.variable
-        )}
-        >
-          <div>
-            <Nav />
-            <main>
-              {children}
-            </main>
-          </div>
-        </body>
+        <StoreProvider>
+          <body className={cn(
+            "min-h-screen bg-background font-manrope antialiased",
+            manrope.variable
+          )}
+          >
+            <div>
+              <Nav />
+              <main>
+                {children}
+              </main>
+            </div>
+          </body>
+        </StoreProvider>
       </html>
     </ReactQueryClientProvider>
   );
