aae3cac00e7781095bcf2aa45f90dc94c5f89d2b
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-04-27 10:47:39 +0530

    add no product Badge for if the selected category has no products available

--- src/app/(home)/components/ShowProducts.tsx
+++ src/app/(home)/components/ShowProducts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 
 import { useQuery } from "@tanstack/react-query"
@@ -6,10 +7,12 @@ import ProductCard from "./ProductCard"
 import { useAppSelector } from "@/lib/redux/hooks"
 import { Category, Product } from "@/types"
 import { Badge } from "@/components/ui/badge"
+import { useEffect, useState } from "react"
 
 const ShowProducts = ({ category }: { category: Category }) => {
 
     const tenantId = (useAppSelector((state) => state.tenant.selectedTenant))?.id
+    const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
 
     const { data: products, isError } = useQuery({
         queryKey: ['products-list', tenantId],
@@ -18,15 +21,24 @@ const ShowProducts = ({ category }: { category: Category }) => {
         },
     })
 
+    useEffect(() => {
+        const dataProducts = products?.data?.data
+        if (dataProducts?.length > 0) {
+            setFilteredProducts(() => dataProducts?.filter((product: Product) => category._id === product.categoryId))
+        }
+    }, [products?.data])
+
 
     return (
         !isError ? products?.data?.data?.length !== 0 ? <div className='sm:grid lg:grid-cols-4 md:grid-cols-3 gap-5 w-full space-y-5 sm:space-y-0'>
             {
-                products?.data?.data.map((product: Product) => {
-                    return category._id === product.categoryId && <div key={product._id}>
-                        <ProductCard product={product} />
-                    </div>
-                })
+                filteredProducts.length > 0
+                    ? filteredProducts.map((product: Product) => {
+                        return <div key={product._id}>
+                            <ProductCard product={product} />
+                        </div>
+                    })
+                    : <Badge variant="secondary">No {category.name} available at the moment. </Badge>
             }
         </div> : <Badge variant="secondary">No products available for this restaurant</Badge> : <Badge variant="destructive">Error Occured</Badge>
     )
