f2871aa48c9fafa5c38320e57e48ccce6f311804
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-01 21:20:32 +0530

    fix window is not defined error by relocating the logic of localStorage to the useEffect hook within the same file by adding some an extra check of storeRef.current

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,7 +2,7 @@
 import { updateCart } from '@/lib/redux/slices/cartSlice'
 import { selectTenant } from '@/lib/redux/slices/selectedTenantSlice'
 import { AppStore, makeStore } from '@/lib/redux/store'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { Provider } from 'react-redux'
 
 export default function StoreProvider({
@@ -10,13 +10,17 @@ export default function StoreProvider({
 }: {
     children: React.ReactNode
 }) {
+
     const storeRef = useRef<AppStore>()
+
     if (!storeRef.current) {
         // Create the store instance the first time this renders
         storeRef.current = makeStore()
+    }
 
+    useEffect(() => {
         // set initial cartItems and selectedTenant on initial render
-        if (typeof window !== undefined && window?.localStorage) {
+        if (typeof window !== 'undefined' && window.localStorage && storeRef.current) {
             try {
                 const localStorageCartItems = window.localStorage.getItem('cartItems')
                 const localStorageSelectedTenant = window.localStorage.getItem('selectedTenant')
@@ -35,9 +39,7 @@ export default function StoreProvider({
                 console.log('Local storage has been modified:', error)
             }
         }
-
-
-    }
+    }, [storeRef])
 
     return <Provider store={storeRef.current}>{children}</Provider>
 }
