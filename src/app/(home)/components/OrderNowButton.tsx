b702ccb497dc4a9cc7c64e2cf4560b643d63ce2a
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-02 13:45:48 +0530

    create order now button

---
+++ src/app/(home)/components/OrderNowButton.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import GoButton from '@/components/custom/GoButton'
+import { Button } from '@/components/ui/button'
+import { useAppSelector } from '@/lib/redux/hooks'
+import Link from 'next/link'
+import React from 'react'
+
+const OrderNowButton = () => {
+
+    const cartItems = useAppSelector((state) => state.cart.cartItems)
+
+    return (
+        <Link href={cartItems.length > 0 ? "/cart" : "#menu"} className="mt-10 grid gap-3 w-full sm:inline-flex">
+            <GoButton title="Order Now" />
+        </Link>
+    )
+}
+
+export default OrderNowButton
+
+
