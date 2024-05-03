ccf713e3ae27af0c440953659721fbc47e8dc2ec
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-03 14:49:27 +0530

    add skeleton while loading the products

--- src/app/(home)/components/ShowProducts.tsx
+++ src/app/(home)/components/ShowProducts.tsx
@@ -8,13 +8,14 @@ import { useAppSelector } from "@/lib/redux/hooks"
 import { Category, Product } from "@/types"
 import { Badge } from "@/components/ui/badge"
 import { useEffect, useState } from "react"
+import MainProductSkeleton from "./MainProductSkeleton"
 
 const ShowProducts = ({ category }: { category: Category }) => {
 
     const tenantId: string | null = (useAppSelector((state) => state.tenant.selectedTenant))?.id
     const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
 
-    const { data: products, isError } = useQuery({
+    const { data: products, isError, isLoading } = useQuery({
         queryKey: ['products-list', tenantId],
         queryFn: () => {
             return getProducts(tenantId)
@@ -30,17 +31,21 @@ const ShowProducts = ({ category }: { category: Category }) => {
 
 
     return (
-        !isError ? products?.data?.data?.length !== 0 ? <div className='sm:grid lg:grid-cols-4 md:grid-cols-3 gap-5 w-full space-y-5 sm:space-y-0 animate-once animate-fade-up animate-duration-[2200] delay-300'>
-            {
-                filteredProducts.length > 0
-                    ? filteredProducts.map((product: Product) => {
-                        return <div key={product._id}>
-                            <ProductCard category={category} product={product} />
-                        </div>
-                    })
-                    : <Badge variant="secondary">No {category.name} available at the moment. </Badge>
-            }
-        </div> : <Badge variant="secondary">No products available for this restaurant</Badge> : <Badge variant="destructive">Error Occured</Badge>
+        !isError ? isLoading
+            ? <div>
+                <MainProductSkeleton forProducts={true} />
+            </div>
+            : products?.data?.data?.length !== 0 ? <div className='sm:grid lg:grid-cols-4 md:grid-cols-3 gap-5 w-full space-y-5 sm:space-y-0 animate-once animate-fade-up animate-duration-[2200] delay-300'>
+                {
+                    filteredProducts.length > 0
+                        ? filteredProducts.map((product: Product) => {
+                            return <div key={product._id}>
+                                <ProductCard category={category} product={product} />
+                            </div>
+                        })
+                        : <Badge variant="secondary">No {category.name} available at the moment. </Badge>
+                }
+            </div> : <Badge variant="secondary">No products available for this restaurant</Badge> : <Badge variant="destructive">Error Occured</Badge>
     )
 }
 
