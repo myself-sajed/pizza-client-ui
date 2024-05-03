a599aa4fedf9a8ffbddefb77c9d0cc701eb3bca1
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-05-03 11:33:05 +0530

    add the suggested / trending items section in cart page for more product interactions

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -9,8 +9,22 @@ import { ArrowRight, CircleX, Cookie, Delete, Pizza, Trash2, Wine } from 'lucide
 import GoButton from '@/components/custom/GoButton'
 import Link from 'next/link'
 import { Badge } from '@/components/ui/badge'
+import { useQuery } from '@tanstack/react-query'
+import { getProducts } from '../(home)/js'
+import { Product } from '@/types'
+import ProductCard from '../(home)/components/ProductCard'
 
 const CartPage = () => {
+
+    const tenantId: string | null = (useAppSelector((state) => state.tenant.selectedTenant))?.id
+
+
+    const { data: products, isError } = useQuery({
+        queryKey: ['products-list', tenantId],
+        queryFn: () => {
+            return getProducts(tenantId)
+        },
+    })
 
     const cartItems: ICartItem[] = useAppSelector((state) => state.cart.cartItems)
     const dispatch = useAppDispatch();
@@ -29,7 +43,7 @@ const CartPage = () => {
 
     return (
         <div className="container my-5">
-            <div className="md:w-2/3 w-full min-h-screen mx-auto">
+            <div className="md:w-4/5 w-full min-h-screen mx-auto animate-once animate-fade-up animate-duration-[2000]">
                 <div className='flex items-center justify-between'>
                     <h1 className='text-lg font-bold space-x-3'><span>Shopping Cart</span> {<Badge className='mb-1'>{cartItems.length}</Badge>} </h1>
                     <div>
@@ -38,7 +52,10 @@ const CartPage = () => {
                         }
                     </div>
                 </div>
-                <div className='bg-white mt-5 p-5 space-y-4 rounded-md animate-once animate-fade-up animate-duration-[2000]'>
+
+
+                {/* SHOPPING CART */}
+                <div className='bg-white mt-5 p-5 space-y-4 rounded-md'>
                     {
                         (cartItems && cartItems?.length) > 0
                             ? (
@@ -55,9 +72,9 @@ const CartPage = () => {
                             )
                             : <div className='w-full flex items-center flex-col my-10'>
                                 <div className='flex items-center justify-center gap-5'>
-                                    <Cookie size={50} className='text-gray-400' />
-                                    <Pizza size={50} className='text-gray-400' />
-                                    <Wine size={50} className='text-gray-400' />
+                                    <Cookie size={30} className='text-gray-400' />
+                                    <Pizza size={30} className='text-gray-400' />
+                                    <Wine size={30} className='text-gray-400' />
                                 </div>
                                 <p className='text-center mb-10 mt-5 text-gray-400'>No items in cart</p>
                                 <Link href="/#menu">
@@ -69,6 +86,23 @@ const CartPage = () => {
 
 
                 </div>
+
+
+                {/* SUGGESTED ITEMS */}
+                {
+                    (products?.data?.data?.length > 0) && <div className='mt-10'>
+                        <h1 className='text-lg font-bold space-x-3 mb-5'><span>{(cartItems && cartItems?.length) !== 0 ? "Trending Items" : "Suggested Items"}</span> {<Badge className='mb-1'>{products?.data?.data?.length}</Badge>} </h1>
+                        <div className='grid grid-cols-4 gap-4 w-full'>
+                            {
+                                products?.data?.data?.map((product: Product) => {
+                                    return <div key={product._id}>
+                                        <ProductCard category={product.category} product={product} />
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     )
