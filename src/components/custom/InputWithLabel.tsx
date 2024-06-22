7a9ba64927989681373833ed21a0acb110073465
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-22 15:43:16 +0530

    update

--- src/components/custom/InputWithLabel.tsx
+++ src/components/custom/InputWithLabel.tsx
@@ -7,15 +7,17 @@ import { Textarea } from "../ui/textarea";
 type PropType = {
     label: string;
     type: string;
-    onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>
+    onChange?: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+    value?: string;
+    disabled?: boolean;
 }
 
-export function InputWithLabel({ label, onChange, type }: PropType) {
+export function InputWithLabel({ label, onChange, type, value = "", disabled = false }: PropType) {
     return (
         <div className="grid w-full items-center gap-1.5">
-            <Label htmlFor={label}>Email</Label>
-            {type === "textarea" ? <Textarea onChange={onChange} id={label} placeholder={label} />
-                : <Input onChange={onChange} type={type} id={label} placeholder={label} />}
+            <Label htmlFor={label}>{label}</Label>
+            {type === "textarea" ? <Textarea onChange={onChange} value={value} id={label} placeholder={label} disabled={disabled} />
+                : <Input onChange={onChange} value={value} type={type} id={label} placeholder={label} disabled={disabled} />}
         </div>
     )
 }
