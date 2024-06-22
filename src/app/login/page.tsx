e46702d45568d0e451bac9eb54c6616b5d3ee374
myself-sajed <shaikhsajedcoolpop@gmail.com>
2024-06-22 15:42:27 +0530

    import login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import getSession from "@/lib/session"
-import LoginForm from "../checkout/components/LoginForm"
+import LoginForm from "./components/LoginForm"
 import { redirect } from "next/navigation"
 
 const LoginPage = async ({ searchParams }: { searchParams: { redirectTo: string | undefined | null } }) => {
